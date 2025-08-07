0cb1eaa401c010d633498a812116a8a7edff6f62
Vinicius Esmeraldo <vinicius.esmeraldodev@gmail.com>
2025-08-07 16:27:13 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.br.desafio.dominio.Bootcamp;
 import br.com.br.desafio.dominio.Curso;
+import br.com.br.desafio.dominio.Dev;
 import br.com.br.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -11,19 +13,45 @@ public class Main {
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso1.setTitulo("Curso JS");
-        curso1.setDescricao("Descrição do Curso JS");
-        curso1.setCargaHoraria(4);
-
-
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição do Curso JS");
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devVinicius = new Dev();
+        devVinicius.setNome("VINICIUS");
+        devVinicius.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Vinicius: " + devVinicius.getConteudosInscritos());
+        devVinicius.progredir();
+        devVinicius.progredir();
+        System.out.println("-->");
+        System.out.println("Conteúdos Inscritos Vinicius: " + devVinicius.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Vinicius: " + devVinicius.getConteudosConcluidos());
+        System.out.println("XP: " + devVinicius.calcularXp());
+
+        System.out.println("###############\n");
+
+        Dev devDante = new Dev();
+        devDante.setNome("DANTE");
+        devDante.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Dante: " + devDante.getConteudosInscritos());
+        devDante.progredir();
+        devDante.progredir();
+        devDante.progredir();
+        System.out.println("-->");
+        System.out.println("Conteúdos Inscritos Dante: " + devDante.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Dante: " + devDante.getConteudosConcluidos());
+        System.out.println("XP: " + devVinicius.calcularXp());
     }
 }
